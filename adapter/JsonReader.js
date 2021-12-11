aa471a50c44b880ab2a27516b37772361bd390ae
Diego Chavez <ingdiegochavez18@gmail.com>
2021-12-11 14:31:51 -0500

    core/adapter-patterns (#6)
    * core/adapter-pattern
    * core/rename-dir-adapter

---
+++ adapter/JsonReader.js
@@ -0,0 +1,8 @@
+class JsonReader{                               // Class JsonReader
+    read(){                                     // Function read
+      //TODO: Implement the read method of the JsonReader class to read a JSON file and return a JSON object. 
+      return '{"Hello": "Diego"}';
+    }
+}
+  
+module.exports = JsonReader;                    // Export JsonReader class
