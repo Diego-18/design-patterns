c233bc3990df04c068a62db91d333ff7d9f0c5bd
Diego Chavez <ingdiegochavez18@gmail.com>
2022-05-19 12:30:18 -0500

    👌 IMPROVE: organization of the directory structure

--- iterator/example/index.js
+++ iterator/example/index.js
@@ -1,10 +1,10 @@
 const Logs = require('./Logs');
 const logs = new Logs();
 
-logs.fetchData();                               
+logs.fetchData();
 
 while(logs.hasNext()){                          // hasNext() is a method of the Logs class
   let currentLog = logs.next();                 // next() is a method of the Logs class
-  
+
   console.log(currentLog);                      // prints the current log
 }
